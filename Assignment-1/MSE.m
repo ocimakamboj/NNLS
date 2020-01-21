07440dabb4736400c675c6f42c175a2f0969e3c0
ocimakamboj <56351128+ocimakamboj@users.noreply.github.com>
2020-01-21 21:44:50 +0530

    Perceptron

--- Assignment-1/MSE.m
+++
@@ -1,7 +0,0 @@
-function mse = MSE(calculated, desired, N)
-mse = 0;
-for i = 1:N
-    mse = mse + (desired(i)-calculated(i))^2;
-end
-mse = mse/N;
-end
--- Assignment-1/onlinePerceptronR.m
+++ Assignment-1/onlinePerceptronR.m
@@ -58,3 +58,26 @@ function [w,acc,emax,nmax] = onlinePerceptronR(N,data,desired,eta,wstart,epochMa
         disp('online mode did not converge');
     end
 end
+
+function mse = MSE(calculated, desired, N)
+mse = 0;
+for i = 1:N
+    mse = mse + (desired(i)-calculated(i))^2;
+end
+mse = mse/N;
+end
+
+%{
+function to calculate accuracy of the perceptron
+Take as input the calculated and desired responses, and the total number of
+points
+%}
+function ans = accuracy(calculated, desired, N)
+ans = 0;
+    for i = 1:N
+        if(calculated(i)~=desired(i))
+            ans = ans+1;
+        end
+    end
+ans = (N-ans)/N*100;
+end
